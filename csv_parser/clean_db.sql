80a1c45ea7216bd3639c146cc5321b7ce3001b04
Nataliia Korop <n.corop08@gmail.com>
2023-11-06 11:15:28 +0100

    changed indecis and p_index to varchar

--- csv_parser/clean_db.sql
+++ csv_parser/clean_db.sql
@@ -10,3 +10,27 @@ alter table pages auto_increment=1;
 alter table issues auto_increment=1;
 alter table volumes auto_increment=1;
 alter table magazines auto_increment=1;
+
+#delete db
+DROP TABLE IF EXISTS prm_d.captions;
+DROP TABLE IF EXISTS prm_d.repro_issue;
+DROP TABLE IF EXISTS prm_d.reproductions;
+DROP TABLE IF EXISTS prm_d.press_blocks;
+DROP TABLE IF EXISTS prm_d.add_info;
+DROP TABLE IF EXISTS prm_d.pages;
+DROP TABLE IF EXISTS prm_d.issues;
+DROP TABLE IF EXISTS prm_d.volumes;
+DROP TABLE IF EXISTS prm_d.magazines;
+DROP TABLE IF EXISTS prm_d.artwork;
+DROP TABLE IF EXISTS prm_d.organization_place;
+DROP TABLE IF EXISTS prm_d.people_organizations;
+DROP TABLE IF EXISTS prm_d.organizations;
+DROP TABLE IF EXISTS prm_d.place_action;
+DROP TABLE IF EXISTS prm_d.people;
+DROP TABLE IF EXISTS prm_d.places;
+DROP TABLE IF EXISTS prm_d.cities;
+DROP TABLE IF EXISTS prm_d.nations;
+DROP TABLE IF EXISTS prm_d.nationalities;
+DROP TABLE IF EXISTS prm_d.actors;
+
+

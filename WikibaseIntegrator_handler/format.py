21bb459bad760b319ab785c3854756da91fc0e3f
Nataliia Korop <n.corop08@gmail.com>
2023-12-10 16:53:30 +0100

    interface for automatic insertion in process

---
+++ WikibaseIntegrator_handler/format.py
@@ -0,0 +1,14 @@
+from wikibaseintegrator import WikibaseIntegrator
+import json
+from wikibaseintegrator.wbi_config import config as wbi_config
+
+wbi_config['USER_AGENT'] = "Boba-buba-Bot/1.0 (https://www.wikidata.org/wiki/User:Boba-buba)"
+
+wbi = WikibaseIntegrator()
+my_first_wikidata_item = wbi.property.get(entity_id='P214')
+
+# to check successful installation and retrieval of the data, you can print the json representation of the item
+
+with open("wikidata.json", 'w', encoding="utf-8") as f:
+    f.write(json.dumps(my_first_wikidata_item.get_json()))
+    f.flush()
--- WikibaseIntegrator_handler/handler.py
+++ WikibaseIntegrator_handler/handler.py
@@ -3,6 +3,8 @@ from wikibaseintegrator.datatypes import Item, Property
 from wikibaseintegrator.models import Claim, Snak, Qualifiers
 from wikibaseintegrator.wbi_config import config as wbi_config
 
+import json
+
 wbi_config['MEDIAWIKI_API_URL'] = 'http://147.231.55.155/w/api.php'
 wbi_config['SPARQL_ENDPOINT_URL'] = 'http://147.231.55.155:8834/proxy/wdqs/bigdata/namespace/wdq/sparql'
 wbi_config['WIKIBASE_URL'] = 'http://147.231.55.155'
@@ -18,50 +20,72 @@ def get_item(id):
 def get_property(id):
     return wbi.property.get(entity_id=id)
 
-def insert_item(): # TODO: udelat obecneji a dodat nejaky format pro vlastnosti
+def insert_new_item(data): # dict
+    # minimal format of the data {'label': 'smth', 'description' : 'smth', 'aliases' : ['smth', ...]}
     item = wbi.item.new()
 
     # Set an english label
-    item.labels.set(language='en', value='Artwork')
+    item.labels.set(language='en', value=data['label'])
 
     # Set a English description
-    item.descriptions.set(language='en', value='aesthetic item or artistic creation; object whose value is its beauty only, not practical usefulness')
-    item.aliases.set(language='en', values=['artwork', 'piece of art', 'art work'])
+    item.descriptions.set(language='en', value=data['description'])
+    item.aliases.set(language='en', values=data['aliases']) #list
 
-    #item.write(allow_anonymous=True)
     itemEnt = item.write(login=login_instance)
-    print(itemEnt)
+    #print(itemEnt)
 
-#insert_item()
+def update_item_time_property(item_id, property_id, time_precision, time_formatted):
+    # https://www.wikidata.org/wiki/Help:Dates for precision and format
 
-def update_item(): #potrebuje id a nejaky usporadany format pro vlastnosti
+    item_to_update = wbi.item.get(item_id)
+    new_snak = Snak(property_number=property_id, datatype="time", datavalue={ "value" : {"time": time_formatted, "timezone": 0, "before": 0, "after": 0, "precision": time_precision, "calendarmodel": "http://www.wikidata.org/entity/Q1985727"}, "type" : "time"})
 
-    """
-    FUNKCI ZNOVU NEPOUSTET DOKUD NEPREDELAM
-    Dodelat ty vlastnosti, asi pro kazdou z nich bude potreba vytvorit pekny format snaku
-    Pak by to slo jen vyplnovat
-    Zatim mam time, tam lze uvest precision, jestli nevime cele datum,
-    Dobry navod k zjisteni foramtu snaku je: zkusit dat statement do wikibase v prohlizeci a potom printnout to zde v jsonu, tam najdu format snaku , aky musi byt.
-    """
-    item_to_update = wbi.item.get("Q2")
-    #April 8, 1973
-    new_snak = Snak(property_number="P3", datatype="time", datavalue={ "value" : {"time": "+1973-04-08T00:00:00Z", "timezone": 0, "before": 0, "after": 0, "precision": 11, "calendarmodel": "http://www.wikidata.org/entity/Q1985727"}, "type" : "time"})
-    #print(new_snak.get_json())
     claim = Claim()
     claim.mainsnak = new_snak
-    #print(claim.get_json())
 
     item_to_update.add_claims(claim)
-    #itemEnt = item_to_update.write(login=login_instance)
+    itemEnt = item_to_update.write(login=login_instance)
     #print(itemEnt.get_json())
 
-#update_item()
-
-def insert_property(property_info): #TODO: to udelat obecneji
-    property = wbi.property.new()
-
-    property.labels.set()
+def update_item_place():
+    #setting instance of place
+    #setting country
+    #coordinates
     return
+
+property_data = {"label" : "place of death", "description" : "most specific known (e.g. city instead of country, or hospital instead of city) death location of a person, animal or fictional character",
+        "aliases" : [ "deathplace", "died in", "death place", "POD", "location of death" ]  }
+
+def insert_new_property(data):
+    new_property = wbi.property.new()
+
+    new_property.labels.set(language='en', value=data['label'])
+    new_property.descriptions.set(language='en', value=data['description'])
+    new_property.aliases.set(language='en', values=data['aliases'])
+    new_property.datatype = "wikibase-item"
+    new_property.write(login=login_instance)
+    return
+
+def update_property_wikidataPID(property_id, wikidataPID):
+    property_to_update = wbi.property.get(property_id)
+    new_snak = Snak(property_number="P5", datatype="external-id", datavalue={"value" : wikidataPID, "type" : "string"})
+
+    claim = Claim()
+    claim.mainsnak = new_snak
+
+    property_to_update.add_claims(claim)
+    itemEnt = property_to_update.write(login=login_instance)
+
+
+#insert_new_property(data=property_data)
+#update_property_wikidataPID("P14")
+
+'''
+with open("prm.json", 'a', encoding="utf-8") as f:
+    f.write(json.dumps(get_property("P13").get_json()))
+    f.write(json.dumps(get_property("P11").get_json()))
+    f.flush()
+'''
 
 
 #Your OAuth consumer has been created.

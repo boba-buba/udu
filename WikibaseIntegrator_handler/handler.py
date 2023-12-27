e50da84e3dd7c34798367f13b4e4d4a50c46718b
Nataliia Korop <n.corop08@gmail.com>
2023-12-27 18:11:42 +0100

    start of the handlers

--- WikibaseIntegrator_handler/handler.py
+++ WikibaseIntegrator_handler/handler.py
@@ -28,71 +28,9 @@ def get_item(id):
 def get_property(id):
     return wbi.property.get(entity_id=id)
 
-'''
-def insert_new_item(data): # dict
-    # minimal format of the data {'label': 'smth', 'description' : 'smth', 'aliases' : ['smth', ...]}
-    item = wbi.item.new()
 
-    # Set an english label
-    item.labels.set(language='en', value=data['label'])
-
-    # Set a English description
-    item.descriptions.set(language='en', value=data['description'])
-    item.aliases.set(language='en', values=data['aliases']) #list
-
-    itemEnt = item.write(login=login_instance)
-    #print(itemEnt)
-
-def update_item_time_property(item_id, property_id, time_precision, time_formatted):
-    # https://www.wikidata.org/wiki/Help:Dates for precision and format
-
-    item_to_update = wbi.item.get(item_id)
-    new_snak = Snak(property_number=property_id, datatype="time", datavalue={ "value" : {"time": time_formatted, "timezone": 0, "before": 0, "after": 0, "precision": time_precision, "calendarmodel": "http://www.wikidata.org/entity/Q1985727"}, "type" : "time"})
-
-    claim = Claim()
-    claim.mainsnak = new_snak
-
-    item_to_update.add_claims(claim)
-    itemEnt = item_to_update.write(login=login_instance)
-    #print(itemEnt.get_json())
-
-def update_item_place():
-    #setting instance of place
-    #setting country
-    #coordinates
-    return
-
-#property_data = {"label" : "place of death", "description" : "most specific known (e.g. city instead of country, or hospital instead of city) death location of a person, animal or fictional character",
-#        "aliases" : [ "deathplace", "died in", "death place", "POD", "location of death" ]  }
-
-def insert_new_property(data):
-    new_property = wbi.property.new()
-
-    new_property.labels.set(language='en', value=data['label'])
-    new_property.descriptions.set(language='en', value=data['description'])
-    new_property.aliases.set(language='en', values=data['aliases'])
-    new_property.datatype = "wikibase-item"
-    new_property.write(login=login_instance)
-    return
-
-def update_property_wikidataPID(property_id, wikidataPID):
-    property_to_update = wbi.property.get(property_id)
-    new_snak = Snak(property_number="P5", datatype="external-id", datavalue={"value" : wikidataPID, "type" : "string"})
-
-    claim = Claim()
-    claim.mainsnak = new_snak
-
-    property_to_update.add_claims(claim)
-    itemEnt = property_to_update.write(login=login_instance)
-
-'''
-'''
-with open("prm.json", 'a', encoding="utf-8") as f:
-    f.write(json.dumps(get_property("P13").get_json()))
-    f.write(json.dumps(get_property("P11").get_json()))
-    f.flush()
-'''
-
+#print(get_item("Q27"))
+get_json_to_file("Q30")
 
 #Your OAuth consumer has been created.
 #Your tokens are:
--- WikibaseIntegrator_handler/volume_handler.py
+++ WikibaseIntegrator_handler/volume_handler.py
@@ -4,56 +4,68 @@ from handler import general_properties
 
 import handler
 
-volume_properties = {"volume" : "P27"}
-volume_qid = "Q10"
-volume_numeric_id = 10
+class Volume:
 
-def volume_insert_new(data):
-    #data = {vol_number : ..., magazine_name : ... }
-    item = wbi.item.new()
-    label = data['magazine'] + ", Vol." + data["vol_number"]
-    item.labels.set(language='en', value=label)
+    volume_properties = {"volume" : "P27", "part_of" : "P29", "title" : "P22"}
+    volume_qid = "Q10"
+    volume_numeric_id = 10
 
-    #description ???
-#######
-    instance_snack = handler.Snack(
-        property_number=general_properties["instance_of"],
-        datatype="wikibase-item",
-        datavalue={
-            "value": {
-                "entity-type": "item",
-                "numeric-id": volume_numeric_id,
-                "id": volume_qid
-            },
-            "type": "wikibase-entityid"
-        }
-    )
-    instance_claim = handler.Claim()
-    instance_claim.mainsnak = instance_snack
-######
-    volume_snack = handler.Snak(
-        property_number=volume_properties["volume"],
-        datatype="string",
-        datavalue={
-            "value": data["vol_number"],
-            "type": "string"
-        }
-    )
-    volume_claim= handler.Claim()
-    volume_claim.mainsnak = volume_snack
-#####
-## publication date 
-# start time
-# end time
+    def volume_insert_new(self, data):
+        #data = {vol_number : ..., magazine_name : ... , publication date start: ..., publication date end: ...} date in what foramt
+        item = wbi.item.new()
+        label = data['magazine'] + ", Vol." + data["vol_number"]
+        item.labels.set(language='en', value=label)
 
-# Q116172661
-#SELECT ?item ?itemLabel ?itemDescription ?book ?bookLabel WHERE {
-#   ?item wdt:P31 wd:Q1238720 .
-#   ?item wdt:P361 ?book .
-#   ?book wdt:P31 wd:Q41298 .
-# SERVICE bla bla
-# }
+        #description ???
+        #######
+        instance_snack = handler.Snack(
+            property_number=general_properties["instance_of"],
+            datatype="wikibase-item",
+            datavalue={
+                "value": {
+                    "entity-type": "item",
+                    "numeric-id": self.volume_numeric_id,
+                    "id": self.volume_qid
+                },
+                "type": "wikibase-entityid"
+            }
+        )
+        instance_claim = handler.Claim()
+        instance_claim.mainsnak = instance_snack
+        ######
+        volume_snack = handler.Snak(
+            property_number=self.volume_properties["volume"],
+            datatype="string",
+            datavalue={
+                "value": data["vol_number"],
+                "type": "string"
+            }
+        )
+        volume_claim=handler.Claim()
+        volume_claim.mainsnak = volume_snack
+    #####
+    ## publication date 
+    # start time
+    # end time
 
-    item.add_claims([instance_claim, volume_claim])
+    # Q116172661
+    #SELECT ?item ?itemLabel ?itemDescription ?book ?bookLabel WHERE {
+    #   ?item wdt:P31 wd:Q1238720 .
+    #   ?item wdt:P361 ?book .
+    #   ?book wdt:P31 wd:Q41298 .
+    # SERVICE bla bla
+    # }
 
-    itemEnt = item.write(login=login_instance)
+        item.add_claims([instance_claim, volume_claim])
+
+        itemEnt = item.write(login=login_instance)
+
+    def volume_handle(self, data): #data magazine title, volume number
+        magazine_name = data['name']
+        magazine_id = self.magazine_in_db(magazine_name=magazine_name)
+        if magazine_id == -1:
+            lang = data['lang']
+            self.magazine_insert_new(data, lang)
+        else:
+            if len(data) > 2: #miminum name and lang
+                self.magazine_update(data)
